42febbab2381f52d0d551784a2b40be76cb0b957
MohdAinul <mohdainul01@gmail.com>
2026-02-01 19:37:34 +0530

    integrate backend

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,32 +1,75 @@
-import React, { useEffect, useRef } from 'react'
-import assets, { messagesDummyData } from '../assets/assets'
+import { useContext, useEffect, useRef, useState } from "react";
+import assets, { messagesDummyData } from "../assets/assets";
 import { formatMessageTime } from "../lib/utils";
+import { ChatContext } from "../../context/ChatContext";
+import { AuthContext } from "../../context/AuthContext";
+import toast from "react-hot-toast";
 
-const ChatContainer = ({selectedUser, setselectedUser}) => {
+const ChatContainer = () => {
+  const { messages, selectedUser, setSelectedUser, sendMessage, getMessages } =
+    useContext(ChatContext);
 
-  const scrollEnd=useRef()
-   useEffect(() => {
-    if (scrollEnd.current) {
+  const { authUser, onlineUsers } = useContext(AuthContext);
+
+  const scrollEnd = useRef();
+
+  const [input, setInput] = useState("");
+
+  // Handle sending a message
+  const handleSendMessage = async (e) => {
+    e.preventDefault();
+    if (input.trim() === "") return null;
+
+    await sendMessage({ text: input.trim() });
+    setInput("");
+  };
+
+  // Handle sending an image
+  const handleSendImage = async (e) => {
+    const file = e.target.files[0];
+    if (!file || !file.type.startsWith("image/")) {
+      toast.error("select an image file");
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onloadend = async () => {
+      await sendMessage({ image: reader.result });
+      e.target.value = "";
+    };
+
+    reader.readAsDataURL(file);
+  };
+
+  useEffect(() => {
+    if (selectedUser) {
+      getMessages(selectedUser._id);
+    }
+  }, [selectedUser]);
+
+  useEffect(() => {
+    if (scrollEnd.current && messages) {
       scrollEnd.current.scrollIntoView({ behavior: "smooth" });
     }
-  }, []);
+  }, [messages]);
+
   return selectedUser ? (
     <div className="h-full overflow-scroll relative backdrop-blur-lg">
-
-      {/*------header-----*/}
+      {/* --------- header --------- */}
       <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
         <img
-          src={assets.profile_martin}
+          src={selectedUser.profilePic || assets.avatar_icon}
           alt="profile"
           className="w-8 rounded-full"
         />
         <p className="flex-1 text-lg text-white flex items-center gap-2">
-          Martin
-          <span className="w-2 h-2 rounded-full bg-green-500"></span>
-          
+          {selectedUser.fullName}
+          {onlineUsers.includes(selectedUser._id) && (
+            <span className="w-2 h-2 rounded-full bg-green-500"></span>
+          )}
         </p>
         <img
-          onClick={() => setselectedUser(null)}
+          onClick={() => setSelectedUser(null)}
           src={assets.arrow_icon}
           alt="arrow"
           className="md:hidden max-w-7"
@@ -37,13 +80,13 @@ const ChatContainer = ({selectedUser, setselectedUser}) => {
           className="max-md:hidden max-w-5"
         />
       </div>
-         {/* --------- chat area --------- */}
+      {/* --------- chat area --------- */}
       <div className="flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6">
-        {messagesDummyData.map((msg, index) => (
+        {messages.map((msg, index) => (
           <div
             key={index}
             className={`flex items-end gap-2 justify-end ${
-              msg.senderId !== '680f50e4f10f3cd28382ecf9'&& "flex-row-reverse"
+              msg.senderId !== authUser._id && "flex-row-reverse"
             }`}
           >
             {msg.image ? (
@@ -55,7 +98,7 @@ const ChatContainer = ({selectedUser, setselectedUser}) => {
             ) : (
               <p
                 className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${
-                  msg.senderId === '680f50e4f10f3cd28382ecf9'
+                  msg.senderId === authUser._id
                     ? "rounded-br-none"
                     : "rounded-bl-none"
                 }`}
@@ -67,9 +110,9 @@ const ChatContainer = ({selectedUser, setselectedUser}) => {
             <div className="text-center text-xs">
               <img
                 src={
-                  msg.senderId === '680f50e4f10f3cd28382ecf9'
-                    ? assets.avatar_icon 
-                    : assets.profile_martin
+                  msg.senderId === authUser._id
+                    ? authUser?.profilePic || assets.avatar_icon
+                    : selectedUser?.profilePic || assets.avatar_icon
                 }
                 alt="profile"
                 className="w-7 rounded-full"
@@ -83,16 +126,19 @@ const ChatContainer = ({selectedUser, setselectedUser}) => {
         <div ref={scrollEnd}></div>
       </div>
 
-
- {/* --------- bottom area --------- */}
+      {/* --------- bottom area --------- */}
       <div className="absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3">
         <div className="flex-1 flex items-center bg-gray-100/12 px-3 rounded-full">
           <input
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+            onKeyDown={(e) => (e.key === "Enter" ? handleSendMessage(e) : null)}
             type="text"
             placeholder="Send a message"
             className="flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400"
           />
           <input
+            onChange={handleSendImage}
             type="file"
             id="image"
             accept="image/png, image/jpeg"
@@ -107,20 +153,19 @@ const ChatContainer = ({selectedUser, setselectedUser}) => {
           </label>
         </div>
         <img
+          onClick={handleSendMessage}
           src={assets.send_button}
           alt="send"
           className="w-7 cursor-pointer"
         />
       </div>
     </div>
-    
   ) : (
     <div className="flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden">
       <img src={assets.logo_icon} alt="logo" className="max-w-16" />
       <p className="text-lg font-medium text-white">Chat anytime, anywhere</p>
     </div>
   );
-  
-}
+};
 
-export default ChatContainer
+export default ChatContainer;
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
-import assets from '../assets/assets'
+import { useContext, useState } from "react";
+import assets from "../assets/assets";
+import { AuthContext } from "../../context/AuthContext";
 
 const LoginPage = () => {
   const [currState, setCurrentState] = useState("Sign up");
@@ -9,104 +10,130 @@ const LoginPage = () => {
   const [bio, setBio] = useState("");
   const [isDataSubmitted, setIsDataSubmitted] = useState(false);
 
+  const { login } = useContext(AuthContext);
 
-    return (
-      <div className="min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl">
-        {/* -------- left -------- */}
-        <img src={assets.logo_big} alt="logo" className="w-[min(30vw,250px)]" />
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
 
-        {/* -------- right -------- */}
-        <form className="border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg">
-          <h2 className="font-medium text-2xl flex justify-between items-center">
-            {currState}
+    if (currState === "Sign up" && !isDataSubmitted) {
+      setIsDataSubmitted(true);
+      return;
+    }
+
+    login(currState === "Sign up" ? "signup" : "login", {
+      fullName,
+      email,
+      password,
+      bio,
+    });
+  };
+
+  return (
+    <div className="min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl">
+      {/* -------- left -------- */}
+      <img src={assets.logo_big} alt="logo" className="w-[min(30vw,250px)]" />
+
+      {/* -------- right -------- */}
+      <form
+        onSubmit={onSubmitHandler}
+        className="border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg"
+      >
+        <h2 className="font-medium text-2xl flex justify-between items-center">
+          {currState}
+          {isDataSubmitted && (
             <img
+              onClick={() => setIsDataSubmitted(false)}
               src={assets.arrow_icon}
               alt="arrow"
               className="w-5 cursor-pointer"
             />
-          </h2>
-          {currState === "Sign up" && !isDataSubmitted && (
+          )}
+        </h2>
+
+        {currState === "Sign up" && !isDataSubmitted && (
+          <input
+            onChange={(e) => setFullName(e.target.value)}
+            value={fullName}
+            type="text"
+            className="p-2 border border-gray-500 rounded-md focus:outline-none"
+            placeholder="Full Name"
+            required
+          />
+        )}
+
+        {!isDataSubmitted && (
+          <>
             <input
-              onChange={(e) => setFullName(e.target.value)}
-              value={fullName}
-              type="text"
-              className="p-2 border border-gray-500 rounded-md focus:outline-none"
-              placeholder="Full Name"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              type="email"
+              placeholder="Email Address"
               required
-            />
-          )}
-          {!isDataSubmitted && (
-            <>
-              <input
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-                type="email"
-                placeholder="Email Address"
-                required
-                className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              />
-              <input
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-                type="password"
-                placeholder="Password"
-                required
-                className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              />
-            </>
-          )}
-
-          {currState === "Sign up" && isDataSubmitted && (
-            <textarea
-              onChange={(e) => setBio(e.target.value)}
-              value={bio}
-              rows={4}
               className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              placeholder="provide a short bio..."
+            />
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              type="password"
+              placeholder="Password"
               required
-            ></textarea>
+              className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            />
+          </>
+        )}
+
+        {currState === "Sign up" && isDataSubmitted && (
+          <textarea
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+            rows={4}
+            className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            placeholder="provide a short bio..."
+            required
+          ></textarea>
+        )}
+
+        <button
+          type="submit"
+          className="py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer"
+        >
+          {currState === "Sign up" ? "Create Account" : "Login Now"}
+        </button>
+
+        <div className="flex items-center gap-2 text-sm text-gray-500">
+          <input type="checkbox" />
+          <p>Agree to the terms of use & privacy policy.</p>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          {currState === "Sign up" ? (
+            <p className="text-sm text-gray-600">
+              Already have an account?{" "}
+              <span
+                onClick={() => {
+                  setCurrentState("Login");
+                  setIsDataSubmitted(false);
+                }}
+                className="font-medium text-violet-500 cursor-pointer"
+              >
+                Login here
+              </span>
+            </p>
+          ) : (
+            <p className="text-sm text-gray-600">
+              Create an account{" "}
+              <span
+                onClick={() => setCurrentState("Sign up")}
+                className="font-medium text-violet-500 cursor-pointer"
+              >
+                Click here
+              </span>
+            </p>
           )}
+        </div>
+      </form>
+    </div>
+  );
+};
 
-          <button
-            type="submit"
-            className="py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer"
-          >
-            {currState === "Sign up" ? "Create Account" : "Login Now"}
-          </button>
-
-          <div className="flex items-center gap-2 text-sm text-gray-500">
-            <input type="checkbox" />
-            <p>Agree to the terms of use & privacy policy.</p>
-          </div>
-          <div className="flex flex-col gap-2">
-            {currState === "Sign up" ? (
-              <p className="text-sm text-gray-600">
-                Already have an account?{" "}
-                <span
-                  onClick={() => {
-                    setCurrentState("Login");
-                    setIsDataSubmitted(false);
-                  }}
-                  className="font-medium text-violet-500 cursor-pointer"
-                >
-                  Login here
-                </span>
-              </p>
-            ) : (
-              <p className="text-sm text-gray-600">
-                Create an account{" "}
-                <span
-                  onClick={() => setCurrentState("Sign up")}
-                  className="font-medium text-violet-500 cursor-pointer"
-                >
-                  Click here
-                </span>
-              </p>
-            )}
-          </div>
-        </form>
-      </div>
-    );
-}
-
-export default LoginPage
+export default LoginPage;
